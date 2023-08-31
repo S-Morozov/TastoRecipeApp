f6b93d88b78a1823ba166636fb2f8808e7743429
Semen Morozov <semenmorozovsuomi@gmail.com>
2023-08-31 17:05:51 +0300

    #started login page development

--- components/screens/auth/Auth.js
+++ components/screens/auth/Auth.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+// eslint-disable-next-line camelcase
+import {useFonts, Inter_700Bold} from '@expo-google-fonts/inter';
 
 const Auth = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    // eslint-disable-next-line camelcase
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
         <Text style={styles.titleText}>Tasto</Text>
+      </View>
+      <View style={styles.smallTitleConatainer}>
+        <Text style={styles.smallTitleText}>Share your best recipe</Text>
       </View>
     </View>
   );
@@ -25,18 +39,25 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
   },
-
   titleText: {
     color: '#AF3D3D',
     textAlign: 'center',
     textShadowColor: 'rgba(0, 0, 0, 0.25)',
     textShadowOffset: {width: 0, height: 4},
     textShadowRadius: 4,
-    fontFamily: 'Inter',
+    fontFamily: 'Inter_700Bold',
     fontSize: 48,
-    fontWeight: '700',
-    lineHeight: 48, // You can specify a numeric value or use '48px' for strings
+    lineHeight: 48,
     letterSpacing: -0.96,
+  },
+
+  smallTitleConatainer: {
+    position: 'absolute',
+    top: 230,
+    width: 259,
+    height: 33,
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
 });
 
